3cb736e89c9372f1a8758fd932c417575639a0a2
analiviamm <livialivia2307@gmail.com>
2024-09-02 19:43:12 -0300

    Create results table from activity endpoint

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class RadioactiveMaterial(models.Model):
     name = models.CharField(max_length=100, db_index=True)
-    constant = models.FloatField(help_text="em mSv.m2/h.GBq", db_index=True)
+    constant = models.FloatField(help_text="em µSv.m2/h.GBq", db_index=True)
 
